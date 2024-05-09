c49197704cf1baadc2f8fbbccecd147b8f292396
Alex <a24dzn@gmail.com>
2024-05-09 19:36:35 +0800

    Update _canvasApi.js

--- src/_canvasApi.js
+++ src/_canvasApi.js
@@ -18,11 +18,12 @@ module.exports = function getCanvases(tracks, accessToken) {
     responseType: 'arraybuffer',
     headers: {
       'accept': 'application/protobuf',
-      'content-type': 'application/x-www-form-urlencoded',
+      'content-type': /*'application/x-www-form-urlencoded'*/'application/protobuf',
       'accept-language': 'en',
       'user-agent': 'Spotify/8.5.49 iOS/Version 13.3.1 (Build 17D50)',
       'accept-encoding': 'gzip, deflate, br',
       'authorization': `Bearer ${accessToken}`,
+      'cookie': 'sp_dc=AQD4YeKbaH8QVg_6Ox7VD5qjN5eHNRuhJw9c3TvnN-lZokwQoQwtt_2uyfMMB36v8VC_EtrChmpFMemkBWLcDp4iwjnFb_BIcDyx_Va-t8xAqE247A3TKJewWDfN_xwlPYHs4PrkZzW1GB3eeyKlpP_5fD2_y3k'
     }
   }
   return axios.post(CANVASES_URL, requestBytes, options)
